c9ed8d71b2295cd1f04f672c0f65b280cf07681e
IHaveTheSourceCode <marcinbureta@gmail.com>
2022-10-08 23:57:55 +0200

    add functionality

--- main.js
+++ main.js
@@ -7,32 +7,9 @@ const container = [];
 // and project containers will hold projects inside of them
 // and projects will hold notes inside of them
 
-class projectsContainer {
-  constructor(container_name, id) {
-    this.container_name = container_name;
-    this.id = id;
-  }
-}
-
-class project {
-  constructor(project_name, id) {
-    this.project_name = project_name;
-    this.id = id;
-  }
-}
-
-class note {
-  constructor(title, description, due_date, starred, id) {
-    this.title = title;
-    this.description = description;
-    this.due_date = due_date;
-    this.starred = starred;
-    this.id = id;
-  }
-}
-
 import {
   callProjectContainerForm,
   createProjectContainer,
 } from "./manipulateDOM";
-createProjectContainer("Project Name2");
+
+createProjectContainer("name");
--- manipulateDOM.js
+++ manipulateDOM.js
@@ -1,45 +1,67 @@
+const mainArray = [];
+
 function callProjectContainerForm() {
   const form = document.querySelector(".project-container-form");
   form.classList.toggle("hide");
+
+  // resets input on call
+  const form_input = document.querySelector("#container-name");
+  form_input.value = "";
 }
 
-//function that send info from above form
-
+// deletes repository inputs
 function unappendProjectForms() {
   document.querySelectorAll(".project-name-input").innerHTML = "";
 }
 
-function createProjectContainer(project_name) {
+let orderIteration = 0;
+let elementId = 0;
+
+// creates projects container
+function createProjectContainer(projectName) {
+  const projects = document.createElement("div");
+  projects.classList.add("projects");
+  projects.dataset.order = orderIteration;
   const wrapper = document.createElement("div");
   wrapper.classList.add("project-wrapper");
+  wrapper.dataset.order = orderIteration;
   const div = document.createElement("div");
   const img = document.createElement("img");
-  img.src = "public/arrow-down.png";
+  img.src = "../arrow-down.png";
+  img.alt = "open notes image.";
+  img.addEventListener("click", function () {
+    toggleProjectSubdirectory(orderIteration);
+  });
+  orderIteration++;
   const paragraph = document.createElement("p");
-  paragraph.textContent = project_name;
+  paragraph.textContent = projectName;
   const button = document.createElement("div");
   button.classList.add("add-project-wrapper");
   const image = document.createElement("img");
   image.classList.add("add-project-button");
-  image.src = "public/btn-plus.png";
+  image.src = "../btn-plus.png";
 
-  appendProjectListener(button, wrapper);
+  appendProjectListener(button, projects);
 
   button.appendChild(image);
   div.appendChild(img);
   div.appendChild(paragraph);
   wrapper.appendChild(div);
-  wrapper.appendChild(button);
+  projects.appendChild(button);
+  wrapper.appendChild(projects);
   document.querySelector("#projects-nav").appendChild(wrapper);
+
+  let prContainer = new projectsContainer(elementId);
+  mainArray.push(prContainer);
+  elementId++;
 }
-// function createProjectForm
-// function createNoteForm
-// function show/hide projects in container
 
-function toggleProjectSubdirectory() {}
+// shows/hides directory list
+function toggleProjectSubdirectory(target) {
+  document.querySelector(`[data-order="${target}"]`).classList.toggle("hide");
+}
 
-//function that send info from below input
-
+//creates input for creating new directories
 function appendProjectListener(btn, parent) {
   const input = document.createElement("input");
   input.type = "text";
@@ -48,6 +70,16 @@ function appendProjectListener(btn, parent) {
 
   input.addEventListener("keydown", (e) => {
     if (e.code == "Enter") {
+      //creates directory and adds it to array on "enter" press
+      let project = document.createElement("div");
+      project.classList.add("project");
+      let project_name = document.createElement("p");
+      project_name.textContent = input.value;
+      project.appendChild(project_name);
+      parent.appendChild(project);
+      input.value = "";
+      parent.appendChild(input);
+      unappendProjectForms();
     }
   });
 
@@ -56,5 +88,49 @@ function appendProjectListener(btn, parent) {
     parent.appendChild(input);
   });
 }
+
+// create Project on form fill
+function addProject() {
+  const btn = document.querySelector(".add-button");
+  const input = document.querySelector("#container-name");
+
+  btn.addEventListener("click", function () {
+    createProjectContainer(input.value);
+  });
+  // input.addEventListener("keydown", (e) => {
+  //   if (e.code == "Enter") {
+  //     createProjectContainer(input.value);
+  //   }
+  // });
+  // needs fix, not working, dk why yet
+
+  input.value = "";
+}
+addProject();
+
+// functions that create backend elements
+
+function projectsContainer(id) {
+  this.id = id;
+  let arr = [];
+}
+
+function project(id) {
+  this.id = id;
+  let arr = [];
+}
+
+function note(id, important, text, due_date) {
+  this.id = id;
+  this.important = important;
+  this.text = text;
+  this.due_date = due_date;
+}
+
+// add project with custom id when creating project with form
+// add subproject to project with same id when creating subproject with text input
+
+// add function that will render notes when clicking on subdir and
+//and change add note behavior
 
 export { callProjectContainerForm, createProjectContainer };
