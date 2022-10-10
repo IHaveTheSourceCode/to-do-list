2c659a515d2c5dafbc69ab1fe80f836eaf33ef1f
IHaveTheSourceCode <marcinbureta@gmail.com>
2022-10-10 23:18:14 +0200

    added all appending functionality

--- manipulateDOM.js
+++ manipulateDOM.js
@@ -1,4 +1,5 @@
 const mainArray = [];
+const testArr = [];
 
 function callProjectContainerForm() {
   const form = document.querySelector(".project-container-form");
@@ -24,9 +25,10 @@ function createProjectContainer(projectName) {
   projects.dataset.order = orderIteration;
   const wrapper = document.createElement("div");
   wrapper.classList.add("project-wrapper");
-  wrapper.dataset.order = orderIteration;
+  wrapper.dataset.orderWrap = orderIteration;
   const div = document.createElement("div");
   const img = document.createElement("img");
+  img.classList.add("arrow-down-image");
   img.src = "../arrow-down.png";
   img.alt = "open notes image.";
   img.addEventListener("click", function () {
@@ -52,7 +54,9 @@ function createProjectContainer(projectName) {
   document.querySelector("#projects-nav").appendChild(wrapper);
 
   let prContainer = new projectsContainer(elementId);
+  console.log(prContainer);
   mainArray.push(prContainer);
+  console.log(mainArray);
   elementId++;
 }
 
@@ -68,6 +72,7 @@ function appendProjectListener(btn, parent) {
   input.classList.add("project-name-input");
   input.placeholder = "Project name";
 
+  // event that will add single directory when pressing enter on input form
   input.addEventListener("keydown", (e) => {
     if (e.code == "Enter") {
       //creates directory and adds it to array on "enter" press
@@ -76,7 +81,28 @@ function appendProjectListener(btn, parent) {
       let project_name = document.createElement("p");
       project_name.textContent = input.value;
       project.appendChild(project_name);
-      parent.appendChild(project);
+      // pushes element to corresponding project's array
+      for (let i = 0; i < mainArray.length; i++) {
+        if (mainArray[i].id == parent.dataset.order) {
+          mainArray[i].arr.push(new subDir(mainArray[i].arr.length));
+          // console.log(mainArray[i].arr[mainArray[i].arr.length + 1]);
+          // let x = mainArray[i].arr.length - 1;
+
+          //mainArray[i].arr[1].arr
+          let z = mainArray[i].arr[mainArray[i].arr.length - 1].arr;
+          console.log(z);
+          // let y = mainArray[i].arr[mainArray[i].length - 1];
+          // console.log(y);
+          // console.log(x);
+          // console.log(mainArray[i].arr[x]);
+          project.addEventListener("click", function () {
+            subDirClickEvents(
+              mainArray[i].arr[mainArray[i].arr.length - 1].arr
+            );
+          });
+        }
+      }
+      parent.prepend(project);
       input.value = "";
       parent.appendChild(input);
       unappendProjectForms();
@@ -112,25 +138,127 @@ addProject();
 
 function projectsContainer(id) {
   this.id = id;
-  let arr = [];
+  this.arr = [];
 }
 
-function project(id) {
+function subDir(id) {
   this.id = id;
-  let arr = [];
+  this.arr = [];
 }
 
-function note(id, important, text, due_date) {
-  this.id = id;
+function createNote(important, text, date) {
   this.important = important;
   this.text = text;
-  this.due_date = due_date;
+  this.date = date;
 }
 
-// add project with custom id when creating project with form
-// add subproject to project with same id when creating subproject with text input
+//render notes (takes subDir notes array as argument)
+function renderNotes(subDir) {
+  // adds addNoteBtn
+  let projectList = document.querySelector(".project-list");
 
-// add function that will render notes when clicking on subdir and
-//and change add note behavior
+  //clears notes
+  document.querySelectorAll(".note").forEach((note) => note.remove());
+
+  //renders notes
+  for (let i = 0; i < subDir.length; i++) {
+    let important = subDir[i].important;
+    let text = subDir[i].text;
+    let date = subDir[i].date;
+    projectList.prepend(makeNote(important, text, date));
+  }
+}
+
+function makeAddNoteBtn() {
+  let btn = document.createElement("button");
+  btn.classList.add("add-note");
+  btn.textContent = "Add note";
+  return btn;
+}
+
+// function removeMakeAddNoteBtn() {
+//   document.querySelector(".add-note").remove();
+// }
+
+//creates form for making notes nodeElements
+function createNoteForm(subDir) {
+  let noteForm = document.createElement("div");
+  noteForm.classList.add("note-form");
+
+  let textInput = document.createElement("input");
+  textInput.type = "text";
+  textInput.classList.add("note-text");
+
+  let dateInput = document.createElement("input");
+  dateInput.type = "date";
+  dateInput.classList.add("note-date");
+  textInput.addEventListener("keydown", (e) => {
+    if (e.code == "Enter") {
+      subDir.push(new createNote(false, textInput.value, dateInput.value));
+      dateInput.value = "";
+      textInput.value = "";
+      renderNotes(subDir);
+    }
+  });
+
+  noteForm.appendChild(textInput);
+  noteForm.appendChild(dateInput);
+
+  return noteForm;
+  // document.querySelector(".project-list").appendChild(noteForm);
+}
+
+function deleteNoteForm() {
+  document.querySelector(".note-form").remove();
+}
+
+// returns note nodeElement
+function makeNote(important, noteText, noteDate) {
+  let wrapper = document.createElement("div");
+  wrapper.classList.add("note");
+
+  let star = document.createElement("img");
+  star.classList.add("star");
+  if (important == true) {
+    star.src = "../star-filled.png";
+  } else {
+    star.src = "../star.png";
+  }
+
+  let text = document.createElement("div");
+  text.classList.add("note-s");
+  text.textContent = noteText;
+
+  let date = document.createElement("div");
+  date.classList.add("due-date");
+  date.textContent = noteDate;
+
+  wrapper.appendChild(star);
+  wrapper.appendChild(text);
+  wrapper.appendChild(date);
+
+  return wrapper;
+}
+
+// clears notes display
+function clearDisplay() {
+  document.querySelector(".project-list").innerHTML = "";
+}
+
+// events that will occur when clicking on subDir
+// notes will be rendered with addNoteBtn inside of it
+// takes notes array as argument
+function subDirClickEvents(subDirTarget) {
+  clearDisplay();
+  // appends form for filling notes to notes container
+  //btn adding must happen first
+  let notesContainer = document.querySelector(".project-list");
+  //add btn that has event listener below
+
+  notesContainer.appendChild(createNoteForm(subDirTarget));
+
+  // renders notes
+  renderNotes(subDirTarget);
+}
 
 export { callProjectContainerForm, createProjectContainer };
